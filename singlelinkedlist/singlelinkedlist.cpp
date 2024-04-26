b08cd318983c603eda7c1dd8955cbd52f635128d
verss21 <arjunafrizal01@gmail.com>
2024-04-26 10:01:47 +0700

    menambahkan prosedur traverse

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -89,3 +89,14 @@ bool listEmpty() {
 	else
 		return false;
 }
+
+void traverse() {
+	if (listEmpty()) {
+		cout << "List Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		Node* currentNode = START;
+		while (currentNode != NULL) {
