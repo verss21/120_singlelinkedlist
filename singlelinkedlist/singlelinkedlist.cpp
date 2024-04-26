ad2397c36d9390ab773fe26b2af290aafba01994
verss21 <arjunafrizal01@gmail.com>
2024-04-26 10:03:09 +0700

    membuat prosedur data

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -105,3 +105,13 @@ void traverse() {
 		}
 	}
 }
+
+void searchData() {
+	if (listEmpty()) {
+		cout << "List Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		int nim;
