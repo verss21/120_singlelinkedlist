648bd89e93994e3e3958e222fdce81f3a1812f74
verss21 <arjunafrizal01@gmail.com>
2024-04-26 10:10:25 +0700

    membuat case

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -171,3 +171,12 @@ int main() {
 			case 3:
 				traverse();
 				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "Pilihan tidak ada" << endl;
+				break;
+			}
